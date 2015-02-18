42e8203edf0fffdc7c53c14128ba6f065208bea7
Charly Riviere <charly@housl.com>
2015-02-18 14:48:09 +0000

    Created Project

---
+++ HouseOfKings.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HouseOfKings.Web.MvcApplication" Language="C#" %>
---
+++ HouseOfKings.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseOfKings.Web.Startup))]
+namespace HouseOfKings.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
